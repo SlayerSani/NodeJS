eda663500db33adfd4f95bb80bec3ece1000cf3f
SlayerSani <sanisourin1999@gmail.com>
2024-03-20 01:17:31 +0530

    02

---
+++ 05javascriptFundamentsInNode/05javascriptFundamentsInNode.js
@@ -0,0 +1,41 @@
+// var x = 20;
+// let y = 30;
+// const z = 40;
+// // z = 100; // error: Assignment to constant variable.
+// console.log(x+y+z); // 90
+
+
+var x =20;
+if (x===20){
+    console.log("matched")
+} else{
+    console.log("not matched");
+}  // matched
+
+var x ='20';
+if (x===20){
+    console.log("matched")
+} else{
+    console.log("not matched");
+}  // not matched
+
+// var x ='20';
+// for (let i = 0; i <= 10; i++) {
+//     console.log(i);
+// }
+// 0
+// 1
+// 2
+// 3
+// 4
+// 5
+// 6
+// 7
+// 8
+// 9
+// 10
+
+
+const arr =[2,3,4.5,6,7,970];
+console.log(arr);  // [2,3,4.5,6,7,970]
+console.log(arr[4]);  // 7
---
+++ 05javascriptFundamentsInNode/index.js
@@ -0,0 +1,4 @@
+let app = require('./app')
+console.log(app);  // { x: 20, y: 30 }
+console.log(app.x);  // 20
+console.log(app.y);  // 30
