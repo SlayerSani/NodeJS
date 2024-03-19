b37f563bcc0786df6e6a43c5ec83351c0c4bdb04
SlayerSani <sanisourin1999@gmail.com>
2024-03-19 23:58:33 +0530

    01

---
+++ 04makeFirstProgram.js
@@ -0,0 +1,19 @@
+console.log(`Subscribe`);  //Subscribe
+
+let a = 20;
+let b = 30;
+var c = 40;
+console.warn(a+b+c)  //90
+
+
+// Q: is console.log in web broweser and noejs is the same or different
+// Ans: The console.log function behaves similarly in web browsers and Node.js, 
+// but there are some differences. In both environments, console.log is used to output 
+// messages to the console. However, in web browsers, the console is typically part of 
+// the developer tools and is primarily used for debugging and monitoring client-side 
+// JavaScript code. In Node.js, the console is a built-in module that provides similar 
+// functionality but is used for server-side JavaScript development. Additionally, 
+// Node.js offers additional console methods and configuration options compared to 
+// web browsers, such as writing console output to files or streams. 
+// Despite these differences, the basic usage of console.log remains consistent across 
+// both environments.
